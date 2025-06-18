ea45c34611fddd9e1c12fe1aa031e7390d859565
Mohammad Imran <projectimran227@gmail.com>
2025-06-18 09:33:33 +0700

    update migrations

--- database/migrations/2025_06_12_033234_create_licenses_table.php
+++ database/migrations/2025_06_12_033234_create_licenses_table.php
@@ -13,9 +13,10 @@ return new class extends Migration
     {
         Schema::create('licenses', function (Blueprint $table) {
             $table->uuid('id')->primary();
+            $table->string('license_id');
             $table->string('license_type', 2);
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email');
             $table->longText('address');
             $table->unsignedBigInteger('province_id');
             $table->foreign('province_id')->references('id')->on('provinces')->onDelete('cascade');
@@ -23,24 +24,27 @@ return new class extends Migration
             $table->foreign('city_id')->references('id')->on('cities')->onDelete('cascade');
             $table->unsignedBigInteger('district_id');
             $table->foreign('district_id')->references('id')->on('districts')->onDelete('cascade');
-            $table->string('postal_code');
+            $table->unsignedBigInteger('sub_district_id');
+            $table->foreign('sub_district_id')->references('id')->on('sub_districts')->onDelete('cascade');
+            $table->unsignedBigInteger('postal_codes');
+            $table->foreign('postal_codes')->references('id')->on('postal_codes')->onDelete('cascade');
             $table->string('phone');
             $table->string('join_date', 10);
             $table->string('expired_date', 10);
             $table->string('contract_agreement_number');
             $table->string('status', 10);
-            $table->unsignedTinyInteger('buiding_type');
-            $table->unsignedTinyInteger('building_status');
-            $table->string('building_rent_expired_date', 10);
-            $table->decimal('building_area', 8, 2);
-            $table->unsignedTinyInteger('building_condition');
-            $table->boolean('building_has_ac');
-            $table->string('instagram');
-            $table->string('facebook_page');
-            $table->string('tiktok');
-            $table->string('youtube');
-            $table->string('google_maps');
-            $table->string('landing_page_student_registration');
+            $table->unsignedTinyInteger('building_type')->nullable();
+            $table->unsignedTinyInteger('building_status')->nullable();
+            $table->string('building_rent_expired_date', 10)->nullable();
+            $table->decimal('building_area', 8, 2)->nullable();
+            $table->unsignedTinyInteger('building_condition')->nullable();
+            $table->boolean('building_has_ac')->nullable();
+            $table->string('instagram')->nullable();
+            $table->string('facebook_page')->nullable();
+            $table->string('tiktok')->nullable();
+            $table->string('youtube')->nullable();
+            $table->string('google_maps')->nullable();
+            $table->string('landing_page_student_registration')->nullable();
         });
     }
 
