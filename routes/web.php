a3109ce6bbaced3f715222c152dfc366a2b030a0
Mohammad Imran <projectimran227@gmail.com>
2025-06-18 09:38:02 +0700

    routes terbaru

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UsersController;
+use App\Http\Controllers\LicensesController;
 
 
 
@@ -30,3 +31,21 @@ Auth::routes();
 Route::get('/home', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 route::resource('/users', UsersController::class);
+
+Route::resource('/licenses', LicensesController::class);
+
+Route::get('/api/cities/{province_id}', function ($province_id) {
+    return \App\Models\City::where('province_id', $province_id)->select('id', 'name')->get();
+});
+
+Route::get('/api/districts/{city_id}', function ($city_id) {
+    return \App\Models\District::where('city_id', $city_id)->select('id', 'name')->get();
+});
+
+Route::get('/api/sub_districts/{district_id}', function ($district_id) {
+    return \App\Models\SubDistrict::where('district_id', $district_id)->select('id', 'name')->get();
+});
+
+Route::get('/api/postal_codes/{sub_district_id}', function ($sub_district_id) {
+    return \App\Models\PostalCode::where('sub_district_id', $sub_district_id)->select('id', 'postal_code')->get();
+});
