46945c6184573f9351c060eb86624b9321020be7
zoelhapner <achmadzulkiflinurrochim@gmail.com>
2025-09-03 12:13:37 +0700

    journal

--- resources/views/journals/index.blade.php
+++ resources/views/journals/index.blade.php
@@ -28,7 +28,18 @@ $(function () {
     const table = $('#journals-table').DataTable({
         processing: true,
         serverSide: true,
-        ajax: "{{ route('journals.index') }}",
+        ajax: {
+                    url: 'https://asm.aharightbrain.com/journals', 
+                    type: 'GET',
+                    xhrFields: {
+                        withCredentials: true 
+                    },
+                    error: function (xhr, error, thrown) {
+                        console.error("❌ AJAX Error:", error, thrown);
+                        console.log("📄 Response Text:", xhr.responseText);
+                        alert("Gagal memuat data! Cek console untuk detail error.");
+                    }
+                },
         columns: [
             { data: 'DT_RowIndex', name: 'DT_RowIndex', orderable: false, searchable: false },
             { data: 'license_type', name: 'licenses.license_type' },
