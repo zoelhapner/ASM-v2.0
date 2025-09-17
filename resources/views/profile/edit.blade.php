eed1101a0f112a6131541b1c5150f5547f40c114
zoelhapner <achmadzulkiflinurrochim@gmail.com>
2025-09-17 09:11:02 +0700

    jurnal

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -169,7 +169,7 @@
 @endsection
 
 @push('js')
-    {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script> --}}
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <script>
     document.addEventListener("DOMContentLoaded", function() {
         // Inisialisasi tooltip Bootstrap
--- resources/views/vendor/tablar/layouts/vertical.blade.php
+++ resources/views/vendor/tablar/layouts/vertical.blade.php
@@ -46,7 +46,7 @@
                 });
             @endif
         </script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+        
         <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
         <script src="https://cdn.datatables.net/v/bs5/dt-2.3.2/fc-5.0.4/fh-4.0.3/datatables.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
