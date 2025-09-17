ba32977f2d8bf4a80e8a2c5f7e8a8346f50cb478
zoelhapner <achmadzulkiflinurrochim@gmail.com>
2025-09-17 15:28:22 +0700

    js

--- resources/views/vendor/tablar/layouts/vertical.blade.php
+++ resources/views/vendor/tablar/layouts/vertical.blade.php
@@ -50,9 +50,9 @@
         <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
         <script src="https://cdn.datatables.net/v/bs5/dt-2.3.2/fc-5.0.4/fh-4.0.3/datatables.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
-        
+
         @vite(['resources/js/app.js'])
 
-        @stack(['js'])
+        @stack('js')
     </body>
 @stop
